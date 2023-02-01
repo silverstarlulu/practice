1fb024d852614031ae4c6feac52b32ad791a0320
99easy <dlwlals0614@gmail.com>
2023-02-01 23:44:06 +0900

    0201 11:44

--- prac/src/main/java/org/example/jimin/prac.java
+++ prac/src/main/java/org/example/jimin/prac.java
@@ -3,5 +3,7 @@ package jimin;
 class prac {
     public static void main(String[] args) {
         System.out.println("9시 50분 파일 생성");
+
+        System.out.println("0201 11tl 43분 파일 수정 - 지민");
     }
 }
